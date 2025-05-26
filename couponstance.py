f9c0433c519f77deac041889800cbea038b3f9d4
owo <owo.x.nyaa@gmail.com>
2025-05-27 00:35:08 +0900

    쿠폰스탕스

--- couponstance.py
+++ couponstance.py
@@ -80,6 +80,7 @@ async def couponstance():
         await page.fill('#UserId', os.getenv("TRICKAL_UID"))
         await page.fill('#CouponCode', coupon_code)
         await page.click('button[onclick="CouponSubmit()"]')
+        await page.wait_for_timeout(500)
         await browser.close()
 
 asyncio.run(couponstance())
